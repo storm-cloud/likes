8b68b623d8d9e8877b1bf3371e87a82cb7f5f6c2
storm-cloud <nicholas_tilton@icloud.com>
2022-05-07 16:04:19 -0400

    Updated

--- LikesJS.js
+++ LikesJS.js
@@ -1,6 +1,18 @@
-var likes = 0;
+var likes1 = 0;
+var likes2 = 0;
+var likes3 = 0; 
 
-function liked(){
-    likes+=1; 
-    document.getElementById("likes").innerHTML = likes; 
+function liked1(){
+    likes1+=1; 
+    document.getElementById("likes1").innerHTML = likes1; 
+}
+
+function liked2(){
+    likes2+=1; 
+    document.getElementById("likes2").innerHTML = likes2; 
+}
+
+function liked3(){
+    likes3+=1; 
+    document.getElementById("likes3").innerHTML = likes3; 
 }
